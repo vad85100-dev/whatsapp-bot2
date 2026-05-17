a4286e192bc4d11df4aed772a94412b7f463d1cc
vad85100-dev <vad85100@gmail.com>
2026-05-17 14:08:00 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ let stats = {
     reportDate: new Date()
 };
 
-const MIN_BALANCE = -5000;
+const MIN_BALANCE = -50000;
 
 const emj = ['0️⃣','1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣','8️⃣','9️⃣','🔟'];
 
